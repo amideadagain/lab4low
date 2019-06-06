9007041fe49666b4c4b55943cfa4ee469f6ca223
kote6725@gmail.com <kote6725@gmail.com>
2019-06-07 00:08:12 +0300

    Добавьте файлы проекта.

---
+++ A/A.cpp
@@ -0,0 +1,12 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+	std::string str = "sd dg dfg gdgfsdfg sdgf";
+	std::remove_if(str.begin(), str.end(), isspace);
+    std::cout << str; 
+}
+
